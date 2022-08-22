404f98bef0bdb3f07ba4e91e3411978bf524fd15
David Bernal <dlbernal@profile.es>
2022-08-22 21:44:56 +0200

    Add p10k.zsh configuration

--- shell/zsh/.zim/init.zsh
+++ shell/zsh/.zim/init.zsh
@@ -7,5 +7,5 @@ source /Users/leobernal/.dotfiles/shell/zsh/.zim/modules/environment/init.zsh
 source /Users/leobernal/.dotfiles/shell/zsh/.zim/modules/input/init.zsh
 source /Users/leobernal/.dotfiles/shell/zsh/.zim/modules/completion/init.zsh
 source /Users/leobernal/.dotfiles/shell/zsh/.zim/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /Users/leobernal/.dotfiles/shell/zsh/.zim/modules/powerlevel10k/powerlevel10k.zsh-theme
 source /Users/leobernal/.dotfiles/shell/zsh/.zim/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
+source /Users/leobernal/.dotfiles/shell/zsh/.zim/modules/powerlevel10k/powerlevel10k.zsh-theme
