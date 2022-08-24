382aa3b9b1fb117f84032c38ac6ef62893ec1149
David Bernal <dlbernal@profile.es>
2022-08-24 10:39:47 +0200

    Add sdkman installer

---
+++ restoration_scripts/sdkman-installer.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+source "$DOTLY_PATH/scripts/core/_main.sh"
+
+if [ ! -d "$HOME/.sdkman" ]; then
+    log::note "🚀 Installing sdkman"
+    curl -s "https://get.sdkman.io" | bash
+    log::success "sdkman installed successfully"
+fi
+
+
+
